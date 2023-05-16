06d4eaf9610e0a16cba91cdde8d3564c3e0cd905
larkz <pizdil56@gmail.com>
2023-05-17 00:05:07 +0300

    just

--- src/lab1.java
+++ src/lab1.java
@@ -6,7 +6,7 @@
 */
 
 import java.util.*;
-public class Main{
+public class lab1{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Вводимые значения могут быть только целочисленными.");
