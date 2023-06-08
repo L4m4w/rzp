c988ad2960c842bb667f5f240dfd6fa766049a0b
Evgen Larkin <62672090+L4m4w@users.noreply.github.com>
2023-06-08 09:58:02 +0300

    Update lab4.java

--- src/lab4.java
+++ src/lab4.java
@@ -8,15 +8,12 @@ public class lab4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Enter first number
         System.out.print("First number: ");
         int num1 = scan.nextInt();
 
-        // Enter second number
         System.out.print("Second number: ");
         int num2 = scan.nextInt();
 
-        // Calculation GCD
         int gcd = findGCD(num1, num2);
         System.out.println("The desired GCD is " + gcd);
 
@@ -29,7 +26,6 @@ public class lab4 {
         scan.close();
     }
 
-    // Euclidean algorithm
     public static int findGCD(int num1, int num2) {
         if (num2 == 0) {
             return num1;
@@ -38,7 +34,6 @@ public class lab4 {
         }
     }
 
-    // Checking GCD on simple
     public static boolean Simple(int number) {
         if (number <= 1) {
             return false;
